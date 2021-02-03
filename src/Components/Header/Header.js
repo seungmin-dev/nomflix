3b0f6c6ae7364b268d7f8517c541eb47047ecfe3
seungmin_dev <seungmin.dev@gmail.com>
2021-02-04 05:16:01 +0900

    #3.0 CSS in React part One

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,11 @@
 import react from "react";
+import "./Header.css";
+// react에서 style을 적용시키는 방법
+// 1. style 폴더를 만들어서 import
+// 2. 각각의 컴포넌트 폴더를 만들고 그 안에서 style 파일을 import
 
 export default () => (
-    <header>
+    <header className="nav">
         <ul>
             <li>
                 <a href="/">Movies</a>
---
+++ src/Components/Header/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header";
+// react에서 style을 적용시키는 방법
+// 1. style 폴더를 만들어서 import
+// 2. 각각의 컴포넌트 폴더를 만들고 그 안에서 style 파일을 import
+
+export default Header;
