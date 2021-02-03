f41cbb9944d0cabb81b09431797c742aef15698b
seungmin_dev <seungmin.dev@gmail.com>
2021-02-04 05:25:21 +0900

    #3.1 CSS in React part Two

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,12 @@
 import react from "react";
-import "./Header.css";
-// react에서 style을 적용시키는 방법
-// 1. style 폴더를 만들어서 import
-// 2. 각각의 컴포넌트 폴더를 만들고 그 안에서 style 파일을 import
+import styles from "./Header.module.css";
+// css를 js처럼 모듈로 불러오는 방법
+// 작은 프로젝트에서 쓰면 좋음
+// 요소검색(?) 해보면 클래스가 익명으로 생성됨
+// but 클래스명을 항상 맞춰줘야하는 불편함이 있음
 
 export default () => (
-    <header className="nav">
+    <header className={styles.navList}>
         <ul>
             <li>
                 <a href="/">Movies</a>
