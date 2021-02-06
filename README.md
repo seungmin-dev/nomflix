b280f2621aa61f74f2b80eeeb4919ad969e7314c
seungmin_dev <seungmin.dev@gmail.com>
2021-02-06 11:22:16 +0900

    networking

--- README.md
+++ README.md
@@ -11,8 +11,11 @@ Learning React and ES6 by building a Movie Discovery App.
 
 ## API Verbs
 
-- [ ] Now Playing (Movie)
-- [ ] Top Rated (TV, Movie)
-- [ ] Popular (TV, Movie)
-- [ ] Up Coming (TV, Movie)
-- [ ] Airing Today (TV)
+- [x] Now Playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (TV, Movie)
+- [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search (TV, Movie)
--- src/api.js
+++ src/api.js
@@ -8,8 +8,35 @@ const api = axios.create({
         language : "en_US"
     }
 });
-api.get("tv/popular"); 
-// tv 앞 '/'로 시작 = 절대경로 = baseURL 을 덮어씀
-// '/'빼고 써야 상대경로
 
-export default api;
+export const moviesApi = {
+    nowPlaying : () => api.get("movie/now_playing"),
+    upcoming : () => api.get("movie/upcoming"),
+    popular : () => api.get("movie/popular"),
+    movieDetail : (id) => api.get(`movie/${id}`, {
+        params : {
+            append_to_response : "videos"
+        }
+    }),
+    search : (term) => api.get('search/movie', {
+        params : {
+            query : encodeURIComponent(term)
+        }
+    })
+}
+
+export const tvApi = {
+    topRated : () => api.get("tv/top_rated"),
+    popular : () => api.get("tv/popular"),
+    airingToday : () => api.get("tv/airing_today"),
+    showDetail : (id) => api.get(`tv/${id}`, {
+        params : {
+            append_to_response : "videos"
+        }
+    }),
+    search : (term) => api.get('search/tv', {
+        params : {
+            query : encodeURIComponent(term)
+        }
+    })
+}
