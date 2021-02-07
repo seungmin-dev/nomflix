0fb400cb3b7c42ed5855ecf63a35ee577d61458d
seungmin_dev <seungmin.dev@gmail.com>
2021-02-08 06:02:40 +0900

    #6.9 Detail ontainer part Two

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -41,11 +41,51 @@ const Cover = styled.div`
     border-radius: 5px;
 `;
 
+const Data = styled.div`
+    width: 70%;
+    margin-left: 20px;
+`;
+
+const Title = styled.h3`
+    font-size: 32px;
+    font-weight: 600;
+`;
+
+const ItemContainer = styled.div`
+    margin: 20px 0;
+`;
+
+const Item = styled.span``;
+
+const Divider = styled.span`
+    margin: 0 10px;
+`;
+
+const Overview = styled.p`
+    font-size: 12px;
+    opacity: 0.7;
+    line-height: 1.5;
+    width: 50%;
+`;
+
 const DetailPresenter = ({ result, loading, error }) => loading ? (
     <Loader /> ) : <Container>
         <Backdrop bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`} />
         <Content>
             <Cover bgImage={result.poster_path ? `https://image.tmdb.org/t/p/original${result.poster_path}` : require("../../assets/noPosterSmall.png")} />
+            <Data>
+                <Title>{result.original_title ? result.original_title : result.original_name}</Title>
+                <ItemContainer>
+                    <Item>
+                        {result.release_date ? result.release_date.substring(0,4) : result.first_air_date.substring(0,4)}
+                    </Item>
+                    <Divider>·</Divider>
+                    <Item>
+                        {result.genres && result.genres.map((genre, index) => index === result.genres.length - 1 ? genre.name : `${genre.name} / `)}
+                    </Item>
+                </ItemContainer>
+                <Overview>{result.overview}</Overview>
+            </Data>
         </Content>
     </Container>
 
